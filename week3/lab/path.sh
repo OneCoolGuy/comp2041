d51c77cc7112bda0157fbd714edc9dca9f90e072
Lucas Germano da Silva <germano@poli.ufrj.br>
2015-08-16 17:28:09 +1000

    More Work

---
+++ week3/lab/path.sh
@@ -0,0 +1,23 @@
+#!/bin/sh - 
+#===============================================================================
+#
+#          FILE: path.sh
+# 
+#         USAGE: ./path.sh 
+# 
+#   DESCRIPTION: 
+# 
+#       OPTIONS: ---
+#  REQUIREMENTS: ---
+#          BUGS: ---
+#         NOTES: ---
+#        AUTHOR: YOUR NAME (), 
+#  ORGANIZATION: 
+#       CREATED: 11/08/15 16:30
+#      REVISION:  ---
+#===============================================================================
+
+set -o nounset                              # Treat unset variables as an error
+
+echo $PATH
+
